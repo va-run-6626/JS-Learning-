1b85b26c215b125f0c95198c2d58974177ec59c5
Varun Bhutada <82926608+va-run-6626@users.noreply.github.com>
2024-08-26 13:34:51 +0000

    memory

--- 01_basics/05_UnderstandingMemory.js
+++ 01_basics/05_UnderstandingMemory.js
@@ -12,7 +12,17 @@ anotherName = "bhutada";
 console.log(anotherName);
 console.log(myname);
 
-
 //Heap
 // - Heap
 // - original reference
+
+let userOne = {
+  email: "user@user.com",
+  upi: "user@ybl",
+};
+
+let userTwo = userOne;
+userTwo.email = "user21234@user.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
