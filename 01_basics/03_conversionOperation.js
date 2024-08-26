8854d19a748340ae19004908e9a4203ff586113a
Varun Bhutada <82926608+va-run-6626@users.noreply.github.com>
2024-08-26 09:34:19 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "43";
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+
+score = "43asc";
+valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33avd" => NaN
+// true => 1
+// false => 0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 ==> true ; 0 ==> false
+// "" => false "hitesh" => true
