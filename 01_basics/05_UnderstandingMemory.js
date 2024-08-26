ddeb650f560806958979e15eea03bb4b7954d9c1
Varun Bhutada <82926608+va-run-6626@users.noreply.github.com>
2024-08-26 10:23:10 +0000

    understanding memory

---
+++ 01_basics/05_UnderstandingMemory.js
@@ -0,0 +1,18 @@
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack
+// - Primitive
+// - copy
+
+let myname = "Varun";
+let anotherName = myname;
+
+anotherName = "bhutada";
+
+console.log(anotherName);
+console.log(myname);
+
+
+//Heap
+// - Heap
+// - original reference
