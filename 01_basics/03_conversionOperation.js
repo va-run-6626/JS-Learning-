4de591743e32831a1f209d3131c6ef5f0fdb4c29
Varun Bhutada <82926608+va-run-6626@users.noreply.github.com>
2024-08-26 09:54:31 +0000

    operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,7 +21,39 @@ valueInNumber = Number(score);
 let isLoggedIn = 1;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 ==> true ; 0 ==> false
 // "" => false "hitesh" => true
+
+
+// ************************************* Operations *************************************
+
+let value = 3;
+let negValue = -value;
+
+console.log(negValue);
+
+let str1 = "hello";
+let str2 = " Varun";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 2 + "2");
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+
+console.log(gameCounter);
+++gameCounter;
