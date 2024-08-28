b7e72cec6bbc356d03097b4f5f36a8af405b5367
Varun Bhutada <82926608+va-run-6626@users.noreply.github.com>
2024-08-28 19:54:16 +0000

    objects part 2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,56 @@
+const tinderUser = new Object();
+// const tinderUser = {};
+
+tinderUser.id = "123abc";
+tinderUser.name = "Sam";
+tinderUser.isLoggedIn = false;
+
+// console.log(tinderUser);
+
+const regularUser = {
+  email: "some@gmail.com",
+  fullName: {
+    userFullName: {
+      firstName: "Varun",
+      lastMName: "Bhuatad",
+    },
+  },
+};
+
+console.log(regularUser.fullName.userFullName);
+
+const obj1 = {
+  1: "a",
+  2: "b",
+};
+const obj2 = {
+  3: "a",
+  4: "b",
+};
+
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({}, obj1, obj2);
+
+const obj3 = { ...obj1, ...obj2 };
+// console.log(obj3);
+
+const users = [
+  {
+    id: 1,
+    email: "a@123.com",
+  },
+  {
+    id: 2,
+    email: "b@342.com",
+  },
+];
+
+console.log(users[1].email);
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty("name"));
